baa8e07fb10b0dcdbb82e1a58630846b0e267ead
Alexandre Andrade <kaniabi@gmail.com>
2021-01-06 16:14:04 -0300

    Add option --prefix.

--- setup.py
+++ setup.py
@@ -4,30 +4,45 @@ from os import popen
 from setuptools import setup, find_packages
 import re
 
-package = 'url2env'
-
-version = popen('git describe --tags').read().strip()
-if not re.match(r'^\d+\.\d+(\.\d+)?$', version):
-    raise Exception('unexpected version: %s' % version)
-
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
 
 setup(
-    name=package,
-    version=version,
+    name="zerotk.url2env",
+    use_scm_version=True,
+
+    author="Alexandre Andrade",
+    author_email='kaniabi@gmail.com',
+    # author="Stuart Campbell",
+    # author_email="stuart@harto.org",
+
+    url="https://github.com/zerotk/url2env",
+
+    description="Produces env vars from Heroku-style database URLs",
+    long_description=long_description,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords="heroku postgresql shell",
+
     packages=find_packages(),
     entry_points={
-        'console_scripts': [
-            'url2env = url2env:main'
+        "console_scripts": [
+            "url2env=url2env:main"
         ],
     },
 
-    description='Produces env vars from Heroku-style database URLs',
-    long_description=long_description,
-    license='MIT',
-    keywords='heroku postgresql shell',
-    author='Stuart Campbell',
-    author_email='stuart@harto.org',
-    url='https://github.com/harto/url2env',
+    install_requires=[
+        'click',
+    ],
+    setup_requires=['setuptools_scm'],
+    tests_require=[],
+
+    license="MIT",
 )
--- url2env/__init__.py
+++ url2env/__init__.py
@@ -1,47 +1,64 @@
 #!/usr/bin/env python
-
 """
 Transforms Heroku-style database URLs into shell environment variables.
 """
 
-from os.path import basename
-from sys import argv
-try:
-    from urllib.parse import urlparse
-except ImportError:
-    from urlparse import urlparse
+import click
 
-def psql_envs(conn):
-    env_map = {'user': 'PGUSER',
-               'pass': 'PGPASSWORD',
-               'host': 'PGHOST',
-               'port': 'PGPORT',
-               'db': 'PGDATABASE'}
-    return dict((env_map[k], v) for k, v in conn.items() if v)
+SCHEMES = (
+    "postgresql",
+    "postgres",
+    "postgis",
+    "psql",
+)
 
-def to_envs(url):
-    assert url.scheme in ('postgresql', 'postgres', 'psql'), \
-        'unsupported URL scheme: %s' % url.scheme
-    conn = {'user': url.username,
-            'pass': url.password,
-            'host': url.hostname,
-            'port': url.port,
-            'db': url.path[1:]}
-    return psql_envs(conn)
 
-def format_envs(envs):
-    return '\n'.join('%s=%s' % (k, v) for k, v in envs.items())
+@click.command()
+@click.argument("database_url")
+@click.option("--prefix", default="PG")
+def main(database_url, prefix):
+    from os.path import basename
+    from sys import argv
 
-def main():
+    try:
+        from urllib.parse import urlparse
+    except ImportError:
+        from urlparse import urlparse
+
+    def psql_envs(conn):
+        env_map = {
+            "user": f"{prefix}USER",
+            "pass": f"{prefix}PASSWORD",
+            "host": f"{prefix}HOST",
+            "port": f"{prefix}PORT",
+            "db": f"{prefix}DATABASE",
+        }
+        return dict((env_map[k], v) for k, v in conn.items() if v)
+
+    def to_envs(url):
+        assert url.scheme in SCHEMES, "unsupported URL scheme: %s" % url.scheme
+        conn = {
+            "user": url.username,
+            "pass": url.password,
+            "host": url.hostname,
+            "port": url.port,
+            "db": url.path[1:],
+        }
+        return psql_envs(conn)
+
+    def format_envs(envs):
+        return "\n".join("%s=%s" % (k, v) for k, v in envs.items())
+
+    def _exit_with_usage():
+        print("usage: %s <url>" % basename(argv[0]))
+        exit(1)
+
     try:
         url = argv[1]
     except IndexError:
         _exit_with_usage()
     print(format_envs(to_envs(urlparse(url))))
 
-def _exit_with_usage():
-    print('usage: %s <url>' % basename(argv[0]))
-    exit(1)
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    main.main()
